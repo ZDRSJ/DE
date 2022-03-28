57aadadea8131bc5e30d2054fb0631fa025eb2dc
ryucm <ckd12394@gmail.com>
2022-03-28 14:55:36 +0900

    ec2 error

--- home/settings.py
+++ home/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 
 ALLOWED_HOSTS = [
-    ".ap-northeast-2.compute.amazonaws.com",'127.0.0.1','0.0.0.0'
+    ".ap-northeast-2.compute.amazonaws.com",'127.0.0.1','3.38.150.19'
 ]
 
 # Application definition
